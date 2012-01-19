180aae2638bad4f709904285efe51400580b764e
Sonny Mai <sonny@lowfiredanger.com.au>
2012-01-19 21:58:57 +1100

    removed comments

--- templateFromCache/_.templateFromCache.js
+++ templateFromCache/_.templateFromCache.js
@@ -12,35 +12,3 @@ _.mixin({
         return options.url;
     })
 });
-
-
-// _.mixin({
-//     templateFromCache: _.memoize(function(options) {
-//         var template;
-//         if (options.url) {
-//             return this.getTemplate(options.url).then(function(data){
-//                 template =  _.template(data);
-//             });
-//         }
-
-//         return null;
-//     }, function(options) {
-//         _.defaults(options, {url: ''});
-//         return options.url;
-//     }),
-
-//     getTemplate: function(url){
-//         var self, dfr;
-//         self = this;
-//         dfr = $.Deferred();
-
-//         $.ajax({
-//             url: url,
-//             type: 'GET',
-//             success: dfr.resolve,
-//             error: dfr.fail
-//         });
-
-//         return dfr.promise();
-//     }
-// });
